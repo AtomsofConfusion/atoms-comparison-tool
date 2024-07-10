60616ba80b4a34cd05dad6528e9e2f2332d12dc2
lw3266 <131626208+lw3266@users.noreply.github.com>
2024-07-09 22:46:52 -0400

    changes to Click arguments

--- src/__main__.py
+++ src/__main__.py
@@ -3,7 +3,9 @@ from src.cli import parse_args, validate_paths
 from src.comparsion import compare_and_output
 
 def main():
-    input_path, output_path, clojure_path, codeQL_path, coccinelle_path = parse_args()
+    temp_string = None
+    input_path, output_path, clojure_path, codeql_path, coccinelle_path = parse_args(temp_string, temp_string, temp_string, temp_string, temp_string)
+
     try:
         checked_input_path, checked_output_path = validate_paths(input_path, output_path)
     except FileNotFoundError as e:
@@ -13,7 +15,8 @@ def main():
     print(f"Input path: {checked_input_path}")
     print(f"Output path: {checked_output_path}")
     print(f"Clojure_path is:{clojure_path}")
-    
+    print(f"CodeQL_path is:{codeql_path}")
+
     compare_and_output(checked_input_path, checked_output_path, clojure_path)
 
     print("Process completed, check output at: ", checked_output_path)
--- src/cli.py
+++ src/cli.py
@@ -1,35 +1,18 @@
 from pathlib import Path
 import click
 
-def parse_args():
-    # parser = ArgumentParser(description="A simple CLI tool that processes files.")
-    # parser.add_argument(
-    #     'input_path', 
-    #     type=str, 
-    #     help='Path to the input folder. There should already be three subfolders containing source data for CodeQL, Clojure, and Coccinelle.'
-    # )
-    # parser.add_argument(
-    #     'output_path', 
-    #     type=str, 
-    #     nargs='?',
-    #     default=None,  # Default output path if not provided
-    #     help='Path to the output folder. Defaults to ' + str(Path(Path.cwd(), "output"))
-    # )
-
-    # # input("Enter the next three path for Clojure, CodeQL, and Coccinelle...\n")
-
-    # # Arguments to truncate from global path
-    # parser.add_argument("clojure_path", type = str, help="Third argument")
-    # # parser.add_argument("codeQL_path", type = str, help="Fourth argument")
-    # parser.add_argument("coccinelle_path", type = str, help="Fifth argument")
-
-    input_path = click.prompt("Enter the input path", type = click.STRING)
-    output_path = click.prompt("Enter the output path", default=any, type = click.STRING)
-    clojure_path = click.prompt("Enter the clojure relative path", type = click.STRING)
-    codeQL_path = click.prompt("Enter the codeQL relative path", type = click.STRING)
-    coccinelle_path = click.prompt("Enter the coccinelle relative path", type = click.STRING)
-
-    return input_path, output_path, clojure_path, codeQL_path, coccinelle_path
+@click.command
+@click.argument('input_path', type = click.Path())
+@click.argument('output_path', nargs = -1, type = click.Path())
+@click.argument('clojure_path', type = click.STRING)
+@click.argument('codeql_path', type = click.STRING)
+@click.argument('coccinelle_path', type = click.STRING)
+def parse_args(input_path, output_path, clojure_path, codeql_path, coccinelle_path):
+    if len(output_path)==0:
+        output_path = None
+    else:
+        output_path = output_path[0]
+    return input_path, output_path, clojure_path, codeql_path, coccinelle_path
 
 def validate_paths(input_path: str, output_path: str) -> None:
     input_path = Path(input_path)
@@ -40,10 +23,9 @@ def validate_paths(input_path: str, output_path: str) -> None:
     if output_path:
         output_path = Path(output_path)
         if not output_path.is_dir():        
-            raise FileNotFoundError(f"Output path does not exist: {output_path}")
+            output_path.mkdir(parents = True, exist_ok = True)
+            print(f"Attention: new folder created at: {output_path}")
 
-        output_path.mkdir(parents = True, exist_ok = True)
-    
     if not output_path:
         output_path = Path(Path.cwd(), "output")
 
