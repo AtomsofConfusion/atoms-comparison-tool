03beec13304bcbcc2971e463712c2b0a176f03b8
lw3266 <131626208+lw3266@users.noreply.github.com>
2024-08-14 07:41:12 -0400

    Update comparsion.py
    Two main goals upon request in this commit:
     1. Allows for comparison between two of the three sources (CQL, CNL, and CLJ). It is still possible to include all three like originally.
    2. Keep only rows that contain two or more '+', so that we will only see comparisons with more than one tool reporting a positive matching.
    
    How the new features will be explained in the next few comments where I can high light sections of the code.
    
    I found it to be trick to "align" the files and make the detection of two or more + work, as the file paths need to be exactly the same across any two sources.
    
    Thus it is hard to

--- src/comparsion.py
+++ src/comparsion.py
@@ -5,7 +5,6 @@ pd.set_option('display.width', 1000)
 import json
 import re
 from pathlib import Path
-from collections import Counter
 
 # orginally clojure_mapping
 combined_mapping = {
@@ -25,11 +24,28 @@ combined_mapping = {
     'literal-encoding' : 'literal_encoding'
 }
 
-def compare_and_output(input_directory, output_directory, codeql_system_path, clojure_system_path, coccinelle_system_path):
+CLJnCQL = ["CLJ", "CQL"]
+CLJnCNL = ["CLJ", "CNL"]
+CQLnCNL = ["CQL", "CNL"]
+all = ["CLJ", "CQL", "CNL"]
+
+def count_plus_specific_columns(row, columns):
+    return (row[columns] == '+').sum()
+
+def compare_and_output(input_directory, output_directory, codeql_system_path, clojure_system_path, coccinelle_system_path, whichtwo):
     codeQL_directory = Path(input_directory, "codeQL_data")
     clojure_directory = Path(input_directory, "clojure_data/atomsClojure.csv")
     coccinelle_directory = Path(input_directory, "coccinelle_data")
     dataframes = {}
+    which_two = []
+    if whichtwo == "CQLnCNL":
+        which_two = CQLnCNL
+    elif whichtwo == "CLJvCQL":
+        which_two = CLJnCQL
+    elif whichtwo == "CLJvCNL":
+        which_two = CLJnCNL
+    else:
+        which_two = all
 
     # Load CodeQL .sarif files and store them in dataframes dictionary
 
@@ -43,7 +59,7 @@ def compare_and_output(input_directory, output_directory, codeql_system_path, cl
         # These are the index paths of the data we are looking for. Each is located inside nested dictionaries.
         for i in range(len(json_value["runs"][0]["results"])):
             s = pd.Series({
-                "Type": filename[:-6],
+                "Type": filename,
                 "File": json_value["runs"][0]["results"][i]["locations"][0]["physicalLocation"]["artifactLocation"]["uri"], 
                 "Line": json_value["runs"][0]["results"][i]["locations"][0]["physicalLocation"]["region"]["startLine"],
                 "CQL": '+',
@@ -60,6 +76,7 @@ def compare_and_output(input_directory, output_directory, codeql_system_path, cl
         # p["File"] = p["File"].str.split('/').str[-1]
         dataframes[dataframe_key] = p
             
+    # print(dataframes)
     # Load Clojure dataset and print the atoms
 
     clojure_master = pd.read_csv(clojure_directory)
@@ -98,7 +115,6 @@ def compare_and_output(input_directory, output_directory, codeql_system_path, cl
         p = pd.read_csv(file_path)
         p.columns = ["Type", "File", "Line", "CNL_Column", "CNL_Code"]
         p["File"] = p["File"].apply(lambda x: re.sub(coccinelle_system_path, '', x, count=1))
-        # p["File"] = p["File"].str.split('/').str[-1]
         p["CNL"] = '+'
         coccinelle_dataframes[dataframe_key] = p
 
@@ -121,15 +137,22 @@ def compare_and_output(input_directory, output_directory, codeql_system_path, cl
         k["CQL_Column"] = k["CQL_Column"].astype(float)
         dataframes[key] = k.sort_values(by = ["File", "Line"]).reset_index().drop("index", axis = 1)
 
-    # Filter out rows with zero or one "+" for "CLJ", "CQL", and "CNL" combined
-    
-    for item, item_df in dataframes.items():
-        item_df = item_df.reindex(columns=["Type", "File", "Line", "CLJ", "CQL", "CNL", "CLJ_Code", "CQL_Column", "CQL_Code", "CNL_Column", "CNL_Code"])
-        dataframes[item] = item_df[(item_df[['CLJ', 'CQL', 'CNL']] == '+').sum(axis=1) >= 2]
-
     # Output merged CSVs to output_directory
 
     for key, df in dataframes.items():
+        df = df.reindex(columns=["Type", "File", "Line", "CLJ", "CQL", "CNL", "CLJ_Code", "CQL_Column", "CQL_Code", "CNL_Column", "CNL_Code"])
+        df = pd.DataFrame(df)
+        if whichtwo == "CLJvCQL":
+            df = df.drop(['CNL', 'CNL_Column', 'CNL_Code'], axis = 1)
+        elif whichtwo == "CLJvCNL":
+            df = df.drop(['CQL', 'CQL_Column', 'CQL_Code'], axis = 1)
+            None
+        elif whichtwo == "CQJvCNL":
+            df = df.drop(['CLJ', 'CLJ_Code'], axis = 1)
+        else:
+            None
+        
+        df = df[df.apply(count_plus_specific_columns, axis=1, columns=which_two) >= 2]
         file_name = f'{key}_comparison.csv'
         path = Path(output_directory, file_name)
         df.to_csv(path, index = False)
