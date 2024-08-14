5b71d7ae4cd0b7f7e9b5e11e975e0e599a57a8ea
lw3266 <131626208+lw3266@users.noreply.github.com>
2024-08-14 07:33:31 -0400

    Update cli.py
     - Added more information to --help.
     - Changed options to accommodate for --whichtwo, that allows for comparison between only two of the three tools.

--- src/cli.py
+++ src/cli.py
@@ -25,22 +25,21 @@ def validate_paths(input_path: str, output_path: str) -> None:
     return input_path, output_path
 
 @click.command()
-@click.option('--input', type = str)
-@click.option('--output', type = str)
-@click.option('--codeql', type = str)
-@click.option('--clojure', type = str)
-@click.option('--coccinelle', type = str)
-# @click.option(help='You will provide five arguments corresponding to 1) input_path, 2) output_path, 3) codeql_path, 4) clojure_path, 5) coccinelle_path. The output_path can be optional by inputting "" indicating an empty string.')
-
-
-def cli(input, output, codeql, clojure, coccinelle):
-
+@click.option('--input', type = str, required = True, help = 'Directory containing three folders of source data.')
+@click.option('--output', type = str, help = 'Directory to store output files. The input is optional.')
+@click.option('--codeql', type = str, required = True, help = 'Relative path to truncate from file names in the CodeQL data.')
+@click.option('--clojure', type = str, help = 'Relative path to truncate from file names in the Clojure data.')
+@click.option('--coccinelle', type = str, required = True, help = 'Relative path to truncate from file names in the Coccinelle data.')
+@click.option('--whichtwo', type = str, required = True, help = "This option selects two, or all, of the three tools to be compared. Four possible inputs are supported: 'CLJvCQL', 'CLJvCNL', 'CQLnCNL', and 'all'. Anything else will behave as the 'all' option.")
+def cli(input, output, codeql, clojure, coccinelle, whichtwo):
+    """The Atoms Comparison tool gathers and compares output from three sources previously developed by the team. Please refer to the README page for more info on each source. To use this project, you will enter four mandatory and two optional options. The usage of each option is listed below."""
     try:
         checked_input_path, checked_output_path = validate_paths(input, output)
     except FileNotFoundError as e:
         print(e)
         sys.exit(1)
 
-    compare_and_output(checked_input_path, checked_output_path, codeql, clojure, coccinelle)
+
+    compare_and_output(checked_input_path, checked_output_path, codeql, clojure, coccinelle, whichtwo)
 
     print(f"Process completed, check output at: \033[1m{checked_output_path}\033[0m")
