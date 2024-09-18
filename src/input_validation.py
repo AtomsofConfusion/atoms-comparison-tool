a31ff6ea5b218f06236d07bdfbba74a840a9ebe9
lw3266 <131626208+lw3266@users.noreply.github.com>
2024-09-17 22:23:40 -0400

    Update input_validation.py

--- src/input_validation.py
+++ src/input_validation.py
@@ -1,19 +1,26 @@
 from pathlib import Path
 
-def validate_CQL(path) -> bool:
+def validate_csv(path) -> bool:
+    if not any(path.glob('*.csv')):
+        return False
+    return True
+
+def validate_codeql(path) -> bool:
     if not any(path.glob('*.sarif')):
         return False
     return True
 
-def validate_CLJ(path) -> bool:
-    if not any(path.glob('*.csv')):
-        return False
-    return True
+# these may be removed if later clojure and coccinelle inputs still only contains csv files
 
-def validate_CNL(path) -> bool:
-    if not any(path.glob('*.csv')):
-        return False
-    return True
+# def validate_clojure(path) -> bool:
+#     if not any(path.glob('*.csv')):
+#         return False
+#     return True
+
+# def validate_coccinelle(path) -> bool:
+#     if not any(path.glob('*.csv')):
+#         return False
+#     return True
 
 ### CHANGE
 # add more functions here if more tools is coming
